ee5690e51cbf4cddee85b651cef2f2e1376a27ad
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2021-10-25 12:24:23 +0200

    Create WardenReason enum

---
+++ src/class/enum/wardenreason.lua
@@ -0,0 +1,14 @@
+--[[
+  This file is part of FlashLand.
+  Created at 25/10/2021 12:23
+  
+  Copyright (c) FlashLand - All Rights Reserved
+  
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Pablo_1610
+
+_FlashEnum_WARDENREASON = {
+
+}
