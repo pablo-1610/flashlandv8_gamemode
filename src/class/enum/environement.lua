ca69edfa40b0a1f56a369bd90c4c7a3986cd8fb6
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2021-10-25 11:17:48 +0200

    Add environement enum

---
+++ src/class/enum/environement.lua
@@ -0,0 +1,15 @@
+--[[
+  This file is part of FlashLand.
+  Created at 25/10/2021 10:48
+  
+  Copyright (c) FlashLand - All Rights Reserved
+  
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Pablo_1610
+
+_FlashEnum_ENV = {
+    DEV = "DEV",
+    PROD = "PROD"
+}
