6b92c1be1b8b6827aaf563d137b942229d339d31
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2021-10-25 12:23:00 +0200

    Populate Ranks component

---
+++ src/components/ranks/server/main.lua
@@ -0,0 +1,50 @@
+--[[
+  This file is part of FlashLand.
+  Created at 25/10/2021 11:52
+  
+  Copyright (c) FlashLand - All Rights Reserved
+  
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Pablo_1610
+
+---@class _FlashServer_Ranks
+_FlashServer_Ranks = {}
+
+local list = {}
+
+_FlashServer_Ranks.exists = function(rankId)
+    return (list[rankId] ~= nil)
+end
+
+_FlashServer_Ranks.add = function(rankId, rank)
+    list[rankId] = rank
+end
+
+_FlashServer_Ranks.get = function(rankId)
+    if (not (_FlashServer_Ranks.exists(rankId))) then
+        return nil
+    end
+    return (list[rankId])
+end
+
+_FlashLand.onReceiveWithoutNet("loaded", function()
+    _FlashServer_Database.query("SELECT flash_ranks.id,flash_ranks.label,flash_ranks.weight,flash_ranks_permissions.permission FROM flash_ranks INNER JOIN flash_ranks_permissions ON flash_ranks.id = flash_ranks_permissions.rankId", {}, function(result)
+        for row, data in pairs(result) do
+            if (_FlashServer_Ranks.exists(data.id)) then
+                ---@type _Rank
+                local rank = _FlashServer_Ranks.get(data.id)
+                rank:addPermission(data.permission)
+            else
+                ---@type _Rank
+                local rank = _Rank(data.id, data.label, data.weight)
+                _FlashServer_Ranks.add(data.id, rank)
+                _FlashLand.log(("Ajout d'un rang: ^2%s"):format(data.id))
+                rank:addPermission(data.permission)
+            end
+        end
+    end)
+end)
+
+_FlashLand.log("Chargement du composant: ^3ranks")
