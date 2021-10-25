c11142984a7be615e814ec60b3e30536ec35f077
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2021-10-25 10:10:29 +0200

    Init project

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+--[[
+  This file is part of FlashLand.
+  Created at 25/10/2021 10:06
+  
+  Copyright (c) FlashLand - All Rights Reserved
+  
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Pablo_1610
+
+fx_version 'cerulean'
+games { 'gta5' };
+
+description "Main system of Flash Land RP"
+author "Pablo_1610"
+version '0.1'
+repository 'https://github.com/PABLO-1610/flashland'
+
+lua54 'yes'
