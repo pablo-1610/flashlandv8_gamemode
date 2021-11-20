7b4fb1d950b38f28b3b56397b7e714564ce2b8c4
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2021-11-20 16:50:00 +0100

    Update webhook

---
+++ src/class/enum/webhook.lua
@@ -0,0 +1,18 @@
+--[[
+  This file is part of FlashLand.
+  Created at 20/11/2021 16:49
+  
+  Copyright (c) FlashLand - All Rights Reserved
+  
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Pablo_1610
+
+_FlashEnum_WEBHOOK = {
+    JOIN_LEFT = "",
+    ADMIN = "",
+    INVENTORIES = "",
+    WARDEN = "",
+    SANCTIONS = ""
+}
