f3b1e572f74de4f179b67eadcdabf12cd06b62f1
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2021-12-10 16:40:22 +0100

    Update blips

--- src/addons/task/server/blipUpdater.lua
+++ src/addons/task/server/blipUpdater.lua
@@ -20,26 +20,7 @@ _FlashServer_Task.doBlipUpdater = function()
         for _, blip in pairs(_FlashServer_Blips.getAll()) do
             local distance = #(playerCoords - npc.position)
             if (blip.restricted) then
-                -- Player is subscribed but no longer allowed
-                if (blip:isSubscribed(_src) and not (blip:isAllowed(_src))) then
-                    blip:unsubscribe(_src)
-                    goto continue
-                end
-                -- Blip is everywhere but player is not subscribed
-                if (blip:isEverywhere() and not (blip:isSubscribed(_src))) then
-                    blip:subscribe(_src)
-                    goto continue
-                end
-                -- Blip is not everywhere but player is subscribed and no longer in drawDist
-                if (not (blip:isEverywhere()) and blip:isSubscribed(_src) and distance > blip.drawDist) then
-                    blip:unsubscribe(_src)
-                    goto continue
-                end
-                -- Blip is not everywhere but player is not subscribed and in drawDist
-                if (not (blip:isEverywhere()) and not (blip:isSubscribed(_src)) and distance <= blip.drawDist) then
-                    blip:subscribe(_src)
-                    goto continue
-                end
+                -- TODO : Restriction
                 goto continue
             end
             -- Blip is everywhere and player is not subscribed
@@ -50,6 +31,11 @@ _FlashServer_Task.doBlipUpdater = function()
             -- Blip is not everywhere and player is subscribed but not in range
             if (not (blip:isEverywhere()) and blip:isSubscribed(_src) and not (distance <= blip.drawDist)) then
                 blip:unsubscribe(_src)
+                goto continue
+            end
+            -- Blip is not everywhere and player is not subscribed but in range
+            if (not (blip:isEverywhere()) and not (blip:isSubscribed(_src)) and distance <= blip.drawDist) then
+                blip:subscribe(_src)
                 goto continue
             end
             :: continue ::
