a7b4d7e910ce92835099061add2e08b4157762f3
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2021-12-12 00:27:36 +0100

    Shop first

---
+++ src/class/enum/shoptypedata.lua
@@ -0,0 +1,25 @@
+--[[
+  This file is part of FlashLand.
+  Created at 12/12/2021 00:13
+  
+  Copyright (c) FlashLand - All Rights Reserved
+  
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Pablo_1610
+
+_FlashEnum_SHOPTYPEDATA = {
+    [_FlashEnum_SHOPTYPE.LTD] = {
+        label = "LTD",
+        blipColor = 67,
+        fleesPercentage = 2,
+    },
+
+    [_FlashEnum_SHOPTYPE.ELEVEN] = {
+        label = "24/7",
+        blipColor = 69,
+        fleesPercentage = 5,
+    },
+
+}
