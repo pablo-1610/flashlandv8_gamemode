a35f353baa67fd331700c10afb51b7f105d9cd3e
VibR1cY <belot.dorian63@gmail.com>
2021-12-11 15:38:58 +0100

    finish animation

--- src/addons/playermenu/client/menu_animations.lua
+++ src/addons/playermenu/client/menu_animations.lua
@@ -9,17 +9,19 @@
 --]]
 ---@author Pablo_1610
 
-local animationType = { _FlashEnum_ANIMATIONTYPE.EXPRESSION, _FlashEnum_ANIMATIONTYPE.PROCESS, _FlashEnum_ANIMATIONTYPE.ANIMATION, _FlashEnum_ANIMATIONTYPE.ANIMATION_AND_PROP }
+local categoriesType = { _FlashEnum_ANIMATIONTYPE.EXPRESSION, _FlashEnum_ANIMATIONTYPE.PROCESS, _FlashEnum_ANIMATIONTYPE.ANIMATION }
+local categoriesIndex = 1
+local animationList = { _FlashEnum_ANIMATIONTYPE.ANIMATION_LIST_FESTIVE, _FlashEnum_ANIMATIONTYPE.ANIMATION_LIST_SALUTE, _FlashEnum_ANIMATIONTYPE.ANIMATION_LIST_WORK, _FlashEnum_ANIMATIONTYPE.ANIMATION_LIST_MOOD, _FlashEnum_ANIMATIONTYPE.ANIMATION_LIST_SPORT, _FlashEnum_ANIMATIONTYPE.ANIMATION_LIST_OTHER, _FlashEnum_ANIMATIONTYPE.ANIMATION_LIST_PEGI }
 local animationIndex = 1
 
 _FlashClient_PlayerMenu.drawer[4] = function()
-    RageUI.List("Type : ", animationType, animationIndex, "", {}, true, {
+    RageUI.List("Catégorie : ", categoriesType, categoriesIndex, "", {}, true, {
         onListChange = function(Index)
-            animationIndex = Index
+            categoriesIndex = Index
         end,
     })
-    RageUI.Line()
-    if animationIndex == 1 then
+    if categoriesIndex == 1 then
+        RageUI.Line()
         for _, anim in pairs(_Static_Animation.expression) do
             RageUI.Button(("%s"):format(anim.label), nil, {}, true, {
                 onSelected = function()
@@ -27,7 +29,8 @@ _FlashClient_PlayerMenu.drawer[4] = function()
                 end
             })
         end
-    elseif animationIndex == 2 then
+    elseif categoriesIndex == 2 then
+        RageUI.Line()
         for _, process in pairs(_Static_Animation.process) do
             RageUI.Button(("Démarche %s"):format(process.label), nil, {}, true, {
                 onSelected = function()
@@ -36,9 +39,26 @@ _FlashClient_PlayerMenu.drawer[4] = function()
                 end
             })
         end
-    elseif animationIndex == 3 then
-
-    elseif animationIndex == 4 then
-
+    elseif categoriesIndex == 3 then
+        RageUI.List("Type : ", animationList, animationIndex, nil, {}, true, {
+            onListChange = function(Index)
+                animationIndex = Index
+            end,
+        })
+        RageUI.Line()
+        for i = 1, #_Static_Animation.animation[animationIndex] do
+            local animation = _Static_Animation.animation[animationIndex][i]
+            RageUI.Button(("%s"):format(animation.label), nil, {}, true, {
+                onSelected = function()
+                    if animation.type == "anim" then
+                        _FlashClient_Utils.animation_load(animation.data.lib)
+                        _FlashClient_Utils.ped_animation(PlayerPedId(), animation.data.lib, animation.data.anim)
+                    elseif animation.type == "scenario" then
+                        print("ko")
+                        _FlashClient_Utils.ped_scenario(PlayerPedId(), animation.data.anim)
+                    end
+                end,
+            })
+        end
     end
 end
--- src/class/enum/animationtype.lua
+++ src/class/enum/animationtype.lua
@@ -13,5 +13,12 @@ _FlashEnum_ANIMATIONTYPE = {
     EXPRESSION = "Expression",
     PROCESS = "Démarche",
     ANIMATION = "Animation",
-    ANIMATION_AND_PROP = "Animation avec objet"
+    ANIMATION_LIST_FESTIVE = "Festives",
+    ANIMATION_LIST_SALUTE = "Salutations",
+    ANIMATION_LIST_WORK = "Travail",
+    ANIMATION_LIST_MOOD = "Humeurs",
+    ANIMATION_LIST_SPORT = "Sports",
+    ANIMATION_LIST_OTHER = "Divers",
+    ANIMATION_LIST_PEGI = "PEGI 21"
+
 }
