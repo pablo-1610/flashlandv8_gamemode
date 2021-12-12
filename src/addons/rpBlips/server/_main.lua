1f6c2038524e9e392a5112000d8681551fae7e0e
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2021-12-12 18:57:15 +0100

    Add some RolePlay blips

---
+++ src/addons/rpBlips/server/_main.lua
@@ -0,0 +1,25 @@
+--[[
+  This file is part of FlashLand.
+  Created at 12/12/2021 17:15
+  
+  Copyright (c) FlashLand - All Rights Reserved
+  
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Pablo_1610
+
+local rpBlipsSize = _Config.genericBlipSize
+
+_FlashServer_Blips.createPublic(vector3(3801.07, 4463.06, 4.93), 410, 62, rpBlipsSize, "Port abandonné", true)
+_FlashServer_Blips.createPublic(vector3(454.08, 5571.99, 781.18), 792, 36, rpBlipsSize, "Mont Chiliad", true)
+_FlashServer_Blips.createPublic(vector3(-1633.73, -1047.49, 13.15), 266, 83, rpBlipsSize, "Fête foraine", true)
+_FlashServer_Blips.createPublic(vector3(-2253.14, 281.28, 174.12), 746, 28, rpBlipsSize, "Centre KoRTZ", true)
+_FlashServer_Blips.createPublic(vector3(686.23, 577.7, 130.46), 685, 19, rpBlipsSize, "Théâtre extérieur", true)
+_FlashServer_Blips.createPublic(vector3(2948.49, 2787.04, 40.69), 801, 36, rpBlipsSize, "Mine", true)
+_FlashServer_Blips.createPublic(vector3(-1340.39, -3042.42, 13.94), 584, 12, rpBlipsSize, "Aéroport", true)
+_FlashServer_Blips.createPublic(vector3(1393.33, 3130.05, 40.65), 584, 12, rpBlipsSize, "Aéroport", true)
+_FlashServer_Blips.createPublic(vector3(2030.48, 4759.52, 41.06), 584, 12, rpBlipsSize, "Aéroport", true)
+_FlashServer_Blips.createPublic(vector3(3540.50, 3727.18, 36.44), 499, 13, rpBlipsSize, "Laboratoire de recherche", true)
+_FlashServer_Blips.createPublic(vector3(-2144.57, 3019.30, 32.82), 576, 75, rpBlipsSize, "Base de l'armée", true)
+_FlashServer_Blips.createPublic(vector3(2740.06, 1536.56, 57.52), 677, 39, rpBlipsSize, "Usine électrique", true)
