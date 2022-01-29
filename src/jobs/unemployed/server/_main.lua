e43163d7ee253238e92310282d36abc0a0250ef6
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2022-01-29 22:38:24 +0100

    Update SQL Schema

---
+++ src/jobs/unemployed/server/_main.lua
@@ -0,0 +1,10 @@
+--[[
+  This file is part of FlashLand.
+  Created at 29/01/2022 22:27
+  
+  Copyright (c) FlashLand - All Rights Reserved
+  
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Pablo_1610
