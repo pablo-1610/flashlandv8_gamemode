9c3305b724be76219a92c6d3984ad109fe42ae5b
Pablo ZAPATA <pablo.zapata.dev@gmail.com>
2022-01-30 19:46:07 +0100

    Job update: add menu

--- src/components/job/client/_main.lua
+++ src/components/job/client/_main.lua
@@ -10,8 +10,23 @@
 ---@author Pablo_1610
 
 _FlashClient_Job = {}
+_FlashClient_Job.workMenu = {}
 
--- FIXME → This keybind seems to be broken
-_FlashClient_KeyBind.addKey("f6", "Ouvrir le menu de travail", function()
-    _FlashLand.log("WIP")
+---hasMenu
+---@param jobId string
+---@return boolean
+function _FlashClient_Job:hasMenu(jobId)
+    return self.workMenu[jobId] ~= nil
+end
+
+_FlashLand.onReceiveWithoutNet("loaded", function()
+    _FlashClient_KeyBind.addKey("f6", "Ouvrir le menu de travail", function()
+        ---@type _Player
+        local player = _FlashClient_Cache.getPlayer()
+        if (_FlashClient_Job:hasMenu(player.job._job.id)) then
+            _FlashClient_Job.workMenu[player.job]()
+        else
+            _FlashClient_Job:openDefaultWorkMenu()
+        end
+    end)
 end)
